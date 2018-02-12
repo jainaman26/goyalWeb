91ac8fe11294ef83d58d9e40904d5fef7197c129
jainaman26 <mymailbox.aman@gmail.com>
2018-02-13 02:23:10 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -35,6 +35,8 @@ app.config(function($routeProvider) {
 app.controller('myCtrl', function($scope, $location) {
      $location.path('/home'); 
     $scope.redirectToPage2 = function(){
+        $scope.elem = angular.element(document.querySelector("#audio"));
+        $scope.elem.play();
        $location.path('/story2');
     }
     $scope.redirectToPage3 = function(){
