10c5846a2390e79eafc28ae48c1f684903c6b92b
jainaman26 <mymailbox.aman@gmail.com>
2018-02-14 00:58:53 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -54,14 +54,9 @@ app.controller('myCtrl', function($scope, $location, $timeout) {
     }
 });
 app.controller('ctrlSlide1', function($scope, $location, $timeout) {
-    $scope.showLoader = false;
     $timeout( function(){
-        $scope.showLoader = true; 
-         $timeout( function(){
-             $location.path('/story3');
-             }, 5000 );
-          
-        }, 3000 );
+             $location.path('/story3');          
+        }, 3500 );
 });
 app.controller('ctrlSlide2', function($scope, $location, $timeout) {
     $timeout( function(){
